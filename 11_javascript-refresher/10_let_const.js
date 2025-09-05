df393c1b02eb6eeaf897601fdc8d21abcf0a6f11
unknown <bdcpas19@gmail.com>
2025-09-05 22:30:54 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,11 @@
+let name = "Mark";
+const age = 20;
+
+console.log("name (let):", name);
+console.log("age (const):", age);
+
+name = "Updated Mark";
+age = 21;
+
+console.log("name (let):", name);
+console.log("age (const):", age);
