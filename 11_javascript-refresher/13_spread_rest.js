40d9f9db5d3e3e00a1114d5c511853d78fc3c2e6
unknown <bdcpas19@gmail.com>
2025-09-05 22:37:19 +0800

    Add spread and rest activity

---
+++ 11_javascript-refresher/13_spread_rest.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4, 5];
+console.log("New Numbers:", newNumbers);
+
+const user = { name: "Elmer", age: 30 };
+const extendedUser = { ...user, isStudent: true };
+console.log("Extended User:", extendedUser);
+
+// Rest operator
+function sumAll(...nums) {
+  return nums.reduce((total, num) => total + num, 0);
+}
+
+console.log("Sum:", sumAll(1, 2, 3, 4, 5));
