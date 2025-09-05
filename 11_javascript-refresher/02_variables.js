4d2950c93ca3d409cb9d65ae3e2affd154bd2d82
unknown <bdcpas19@gmail.com>
2025-09-05 21:45:03 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,18 @@
+let name = "Mark";
+let age = 20;
+let isStudent = true;
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent);
+
+let num1 = 9;
+let num2 = 5;
+
+console.log("Add:", num1 + num2);
+console.log("Subtract:", num1 - num2);
+console.log("Multiply:", num1 * num2);
+console.log("Divide:", num1 / num2);
+
+console.log('"5" == 5:', "5" == 5);     // true
+console.log('"5" === 5:', "5" === 5);   // false
