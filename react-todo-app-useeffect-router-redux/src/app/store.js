a68d2dcc0f318142aa90c32451e87d4e7cb757dd
unknown <bdcpas19@gmail.com>
2025-11-28 09:30:50 +0800

    Done with Create the Redux store section

--- react-todo-app-useeffect-router-redux/src/app/store.js
+++ react-todo-app-useeffect-router-redux/src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../features/todos/todosSlice";
 
 export const store = configureStore({
-  reducer: {
+  reducer: { 
     todos: todosReducer,
   },
 });
